9d35e3ab236748af52248c5c62f6764724e57656
Vikram Goyal <vikramgoyalsg@gmail.com>
2025-01-24 16:00:50 +0800

    Rename main class and add greeting

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Tyler User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Tyler.java
@@ -0,0 +1,17 @@
+public class Tyler {
+    public static void main(String[] args) {
+        String logo = "  _____      _           \n"
+                + " |_   _|   _| | ___ _ __ \n"
+                + "   | || | | | |/ _ \\ '__|\n"
+                + "   | || |_| | |  __/ |   \n"
+                + "   |_| \\__, |_|\\___|_|   \n"
+                + "       |___/             \n";
+        System.out.println("Hello from\n" + logo);
+        String separator = "____________________________________________________________\n";
+        String greeting = " Hello! I'm Tyler!\n"
+                + " What can I do for you?\n";
+        String farewell = " Bye-bye now!\n";
+        System.out.println(separator + greeting
+                + separator + farewell +separator);
+    }
+}
