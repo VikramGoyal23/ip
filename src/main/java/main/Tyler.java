2804ff6563989ca05cbd6348138f4ae9eb4f474b
Vikram Goyal <vikramgoyalsg@gmail.com>
2025-01-31 14:26:56 +0800

    Add error handling
    The user may provide incorrect input which the
    program may be unable to process.
    
    Let's add error handling for these edge cases.

--- src/main/java/main/Tyler.java
+++ src/main/java/main/Tyler.java
@@ -30,51 +30,65 @@ public class Tyler {
         int i = 0;
 
         loop: while (true) {
-            String input = sc.nextLine();
-            System.out.print(separator);
-            String[] tokens = input.split(" ", 2);
-            String command = tokens[0];
-            switch (command.toLowerCase()) {
-            case "list":
-                for (int j = 0; j < 100; j++) {
-                    if (tasks[j] == null) {
-                        break;
-                    } else {
-                        System.out.println("\t " + (j + 1) + ". " + tasks[j]);
+            try {
+                String input = sc.nextLine();
+                System.out.print(separator);
+                String[] tokens = input.split(" ", 2);
+                String command = tokens[0];
+                switch (command.toLowerCase()) {
+                case "list":
+                    for (int j = 0; j < 100; j++) {
+                        if (tasks[j] == null) {
+                            break;
+                        } else {
+                            System.out.println("\t " + (j + 1) + ". " + tasks[j]);
+                        }
                     }
+                    break;
+                case "bye":
+                    break loop;
+                case "mark":
+                    tasks[Integer.parseInt(tokens[1]) - 1].markAsDone();
+                    break;
+                case "unmark":
+                    tasks[Integer.parseInt(tokens[1]) - 1].markAsUndone();
+                    break;
+                case "todo":
+                    ToDo toDo = new ToDo(tokens[1]);
+                    i = Task.addToList(tasks, i, toDo);
+                    break;
+                case "deadline":
+                    if (!tokens[1].contains("/by") || tokens[1].split("/by ")[1].isBlank()) {
+                        throw new IllegalArgumentException("\t !!Deadline must include a 'by' time!!");
+                    }
+                    Deadline deadline = new Deadline(
+                            tokens[1].split(" /by")[0], tokens[1].split("/by ")[1]);
+                    i = Task.addToList(tasks, i, deadline);
+                    break;
+                case "event":
+                    if (!tokens[1].contains("/from") || !tokens[1].contains("/to")
+                            || tokens[1].split("/from ")[1].split(" /to")[0].isBlank()
+                            || tokens[1].split("/to ")[1].isBlank()
+                    ) {
+                        throw new IllegalArgumentException(
+                                "\t !!Event must include a 'from' time and 'to' time!!");
+                    }
+                    Event event = new Event(tokens[1].split(" /from")[0],
+                            tokens[1].split("/from ")[1].split(" /to")[0],
+                            tokens[1].split("/to ")[1]);
+                    i = Task.addToList(tasks, i, event);
+                    break;
+                default:
+                    throw new IllegalArgumentException("\t !!I'm sorry, I don't know what that means!!");
                 }
-                break;
-            case "bye":
-                break loop;
-            case "mark":
-                tasks[Integer.parseInt(tokens[1]) - 1].markAsDone();
-                break;
-            case "unmark":
-                tasks[Integer.parseInt(tokens[1]) - 1].markAsUndone();
-                break;
-            case "todo":
-                tasks[i] = new ToDo(tokens[1]);
-                System.out.println("\t I've added: \n\t\t" + tasks[i]);
-                i += 1;
-                System.out.println("\t There's now " + i + " task(s) in the list.");
-                break;
-            case "deadline":
-                tasks[i] = new Deadline(tokens[1].split(" /by")[0],tokens[1].split("/by ")[1]);
-                System.out.println("\t I've added: \n\t\t" + tasks[i]);
-                i += 1;
-                System.out.println("\t There's now " + i + " task(s) in the list.");
-                break;
-            case "event":
-                tasks[i] = new Event(tokens[1].split(" /from")[0],
-                        tokens[1].split("/from ")[1].split(" /to")[0],
-                        tokens[1].split("/to ")[1]);
-                System.out.println("\t I've added: \n\t\t" + tasks[i]);
-                i += 1;
-                System.out.println("\t There's now " + i + " task(s) in the list.");
-                break;
-            default:
-                System.out.println("\t I'm sorry, I don't know what that means!");
-                break;
+            } catch (NumberFormatException e) {
+                System.out.println("\t !!Please enter a number as the argument!!");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } catch (NullPointerException e) {
+                System.out.println("\t !!There aren't this many tasks in the list!!");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("\t !!Please provide the correct number of arguments!!");
             }
             System.out.print(separator);
         }
--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -12,6 +12,11 @@ public class Task {
         this.isDone = false;
     }
 
+    /**
+     * Returns status icon of task instance.
+     *
+     * @return Status icon.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
@@ -22,26 +27,52 @@ public class Task {
         isDone = done;
     }
 
+    /**
+     * Sets the completion status of the task as complete.
+     *
+     */
     public void markAsDone() {
         setDone(true);
         System.out.println("\t " + "I've marked this task as done: ");
         System.out.println("\t\t" + this);
     }
 
+    /**
+     * Sets the completion status of the task as incomplete.
+     *
+     */
     public void markAsUndone() {
         setDone(false);
         System.out.println("\t " + "I've marked this task as incomplete: ");
         System.out.println("\t\t" + this);
     }
 
-    public String getDescription() {
+    /**
+     * Returns description of task instance.
+     *
+     * @return Task description.
+     */
+    private String getDescription() {
         return description;
     }
 
-    // Will try to implement properly soon
-    public int addToList(Task[] tasks, int index) {
-        System.out.println("\t I've added: \n\t\t" + tasks[index]);
+    /**
+     * Adds task to array passed in as argument at specified index.
+     * Returns the index of the next position to add a task.
+     *
+     * @param tasks Array to which task must be added.
+     * @param index Position of task to be added.
+     * @param task Task to be added.
+     * @return Next task addition index.
+     * @throws IllegalArgumentException If task description is blank.
+     */
+    public static int addToList(Task[] tasks, int index, Task task) throws IllegalArgumentException {
+        if (task.getDescription().isBlank()) {
+            throw new IllegalArgumentException("\t !!Please add a description for the task!!");
+        }
+        tasks[index] = task;
         index += 1;
+        System.out.println("\t I've added: \n\t\t" + task);
         System.out.println("\t There's now " + index + " task(s) in the list.");
         return index;
     }
